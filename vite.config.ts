c1f03718bc075de059678fbca6a40908de77ea92
uqtri <ungquangtri@gmail.com>
2026-02-18 01:54:17 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
+  // Environment variables prefixed with VITE_ will be exposed to your source code
+  // Access via import.meta.env.VITE_API_GATEWAY_URL
 })
